c31e931fc039ae6ff6cf53a1d4e3bacb08896662
Pavel Reshetnikov <pavelresh@yandex.ru>
2026-02-17 00:03:22 +0500

    feat: implement download link properly

--- handlers/url/download.py
+++ handlers/url/download.py
@@ -1,3 +1,4 @@
+import json
 import yt_dlp
 import asyncio
 import logging
@@ -27,16 +28,16 @@ async def download_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
         logging.error("A link must be a valid url", link)
         return
 
-    download_message = await context.bot.send_message(
-        chat.id, f"Загружаю видео и аудио по ссылке {link}"
-    )
+    download_message = await context.bot.send_message(chat.id, "Скачиваю аудио из видео")
 
     with tempfile.TemporaryDirectory() as tmp_dir:
+        out_path = os.path.join(tmp_dir, "audio.mp3")
         opts = {
             "extract_audio": True,
-            "keep_video": True,
-            "format": "bestvideo+bestaudio",
-            "outtmpl": tmp_dir,
+            "writeinfojson": True,
+            "noplaylist": True,
+            "format": "bestaudio",
+            "outtmpl": out_path,
         }
         with yt_dlp.YoutubeDL(opts) as ytdl:
             try:
@@ -44,16 +45,16 @@ async def download_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
             except Exception as e:
                 await context.bot.send_message(
                     chat.id,
-                    "Почему-то не получилось загрузить видео и аудио, простите 😭",
+                    "Почему-то не получилось загрузить аудио, простите 😭",
                 )
-                logging.error("yt_dlp download failed: {}", e)
+                logging.error("yt_dlp download failed", e)
                 return
 
-        file_names = os.listdir(tmp_dir)
-        for file_name in file_names:
-            if file_name.endswith(".mp3"):
-                await context.bot.send_audio(chat.id, os.path.join(tmp_dir, file_name))
-            else:
-                await context.bot.send_video(chat.id, os.path.join(tmp_dir, file_name))
+        with open(out_path + ".info.json") as metadata_file:
+            metadata = json.load(metadata_file)
+            title = metadata["title"]
+            performer = metadata["uploader"]
+
+        await context.bot.send_audio(chat.id, out_path, title=title, performer=performer)
 
     await download_message.delete()
